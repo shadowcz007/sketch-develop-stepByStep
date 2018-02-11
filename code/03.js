f052c697385500a37708cd87d08b7cc80728a404
shadow <389570357@qq.com>
2018-02-11 10:17:57 +0800

    Update 03.js

--- code/03.js
+++ code/03.js
@@ -14,22 +14,35 @@ for(var i=0;i<strs.length;i++){
 };
 
 
-
 function addTextLine(_strs,_i){
 
 var textLayer=[MSTextLayer new];
 
 var newStr=_strs.join('\n')
-log(newStr)
+
+var _fontSize=20,_margin=10,_y=0,_x=_fontSize;
 
 textLayer.setStringValue(newStr);
 textLayer.name=_i.toString();
 
+(_i==0)?_fontSize=_fontSize*1.5:null;
+(i==1)?_fontSize=8:null;
+
+(_i>1)?_y=_fontSize*2:null;
+(i==1)?_y=_fontSize*3:null;
+
+(i==1)?_x=_x*1.2:null;
+
+textLayer.fontSize=_fontSize;
+
+
 var textFrame=[textLayer frame];
-textFrame.x=_i*20;
-textFrame.y=0;
-textFrame.width=10;
-textFrame.height=200;
+
+textFrame.x=_i*(_x+_margin);
+textFrame.y=_y;
+
+textFrame.width=_fontSize;
+//textFrame.height=200;
 
 page.addLayer(textLayer)
     
@@ -47,12 +60,14 @@ function splitStr(_str){
         var _s=_strs[i],_ns=[];         
 
             for(var j=0;j<_s.length;j++){
-                
-                 log(_s[j].match('\U300a'))
-                _s[j]=_s[j].replace('《','「').replace('》','」');
                  
+                var _sc=_s[j];
+
+                 _s[j].match('《')?_sc='︻':null;
+                 _s[j].match('》')?_sc='︼':null;
+                 //_s[j].match(':')?_sc='-':null;                    
                 
-                _ns.push(_s[j])
+                _ns.push(_sc)
                 
             }; 
 
