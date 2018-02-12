604dbd4c2ebe957caa5a6286b3707021e35e6f9c
shadow <389570357@qq.com>
2018-02-12 17:04:56 +0800

    Update 03-catAndDog.js

--- code/03-catAndDog.js
+++ code/03-catAndDog.js
@@ -4,21 +4,23 @@ var page = [doc currentPage];
 var data = openImageFiles();
 
 var _newArtboard = [MSArtboardGroup new];
-
 _newArtboard.name = (new Date()).getTime().toString();
 
 var _artboardFrame =_newArtboard.frame();
 
 var _ab_w = 0,
-  _ab_h = 0,_w=[doc askForUserInput:@"width" initialValue:@"2000"],_h=[doc askForUserInput:@"height" initialValue:@"200"];;
+    _ab_h = 0,
+    _w=[doc askForUserInput:@"width" initialValue:@"2000"],
+    _h=[doc askForUserInput:@"height" initialValue:@"200"];;
 
 for (var i = 0; i < data.length; i++) {
+  
   var _url = data[i];
 
   //   var _x=Math.random()*1000,_y=Math.random()*1000;
 
   var _x = Math.abs(Math.random() * _w * Math.tanh(i)),
-    _y = Math.abs(Math.random() * _h * Math.cos(i));
+      _y = Math.abs(Math.random() * _h * Math.cos(i));
 
   var _imageLayer = drawImg(_x, _y, _url);
 
@@ -26,6 +28,7 @@ for (var i = 0; i < data.length; i++) {
   _ab_h = Math.max(_imageLayer.height, _ab_h);
 
   _newArtboard.addLayer(_imageLayer.layer);
+  
 };
 
 
@@ -35,21 +38,10 @@ _artboardFrame.setWidth(_ab_w);
 _artboardFrame.setHeight(_ab_h);
 
 
-
-
 var textLayer = drawText(_ab_w, _ab_h, _h*0.05);
 
 _newArtboard.addLayer(textLayer);
-
 page.addLayer(_newArtboard);
-
-
-
-
-
-
-
-
 
 
 
@@ -75,7 +67,10 @@ function drawImg(x, y, _url) {
   var image = [[NSImage alloc] initByReferencingFile:_url];
 
   var img = MSImageData.alloc().initWithImage(image);
-let _w=100,_h=100*image.size().width/image.size().height;
+  
+  let _w=100,
+      _h=100*image.size().width/image.size().height;
+  
   var rect = drawRect(x, y, _w, _h);
 
   img2Fill(rect, img);
@@ -85,9 +80,11 @@ let _w=100,_h=100*image.size().width/image.size().height;
     width: _w + x,
     height: _h + y
   };
+  
 };
 
 function drawRect(x, y, w, h) {
+  
   var nsb = [NSBezierPath bezierPath];
   nsb.appendBezierPathWithRect(NSMakeRect(x, y, w, h));
 
@@ -97,7 +94,7 @@ function drawRect(x, y, w, h) {
   lineShape.name = '12';
 
   var lineShapeStyle = lineShape.style(),
-    fills = lineShapeStyle.fills();
+       fills = lineShapeStyle.fills();
 
   if (fills.count() <= 0) {
     lineShapeStyle.addStylePartOfType(0);
@@ -106,22 +103,25 @@ function drawRect(x, y, w, h) {
   // page.addLayers([lineShape]);
 
   return lineShape
+  
 };
 
 function img2Fill(layer, img) {
+  
   let style = layer.style();
 
   let fill = style.fills()[0];
 
   fill.setFillType(4);
-
   fill.setImage(img);
-
+  
   fill.setPatternFillType(3);
+  //fit
 
 };
 
 function openImageFiles() {
+  
   var fileTypes = [NSArray arrayWithObjects: @"png", @"jpg",@"jpeg"];
   var imageFileNames = [];
   var panel = [NSOpenPanel openPanel];
@@ -132,8 +132,8 @@ function openImageFiles() {
   [panel setAllowedFileTypes: fileTypes];
 
   var isConfirm = [panel runModal];
+  
   //isConfirm
-
   if (isConfirm) {
 
     var firstURL =[[panel URLs] objectAtIndex:0];
